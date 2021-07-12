a58b1f8eda11732a883e17142692a93acccbcdfe
bluething <habib.gunadarma@gmail.com>
2021-07-12 15:18:04 +0700

    New class implement Runnable

---
+++ javathreadmodel/src/main/java/io/github/bluething/java/threadmodel/NewThreadWithRunnableDemo.java
@@ -0,0 +1,11 @@
+package io.github.bluething.java.threadmodel;
+
+public class NewThreadWithRunnableDemo {
+    class NewThreadWithRunnable implements Runnable {
+
+        @Override
+        public void run() {
+
+        }
+    }
+}
