c5947679b374f42baba388cd6f0e758f3d354a1a
bluething <habib.gunadarma@gmail.com>
2021-07-12 15:29:35 +0700

    Start the thread object with start method

--- javathreadmodel/src/main/java/io/github/bluething/java/threadmodel/NewThreadWithRunnableDemo.java
+++ javathreadmodel/src/main/java/io/github/bluething/java/threadmodel/NewThreadWithRunnableDemo.java
@@ -1,6 +1,20 @@
 package io.github.bluething.java.threadmodel;
 
 public class NewThreadWithRunnableDemo {
+    public static void main(String[] args) {
+        NewThreadWithRunnable newThreadWithRunnable = new NewThreadWithRunnable();
+        newThreadWithRunnable.thread.start();
+
+        try {
+            for (int i = 5; i > 0; i--) {
+                System.out.println("Main thread" + i);
+                Thread.sleep(500);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Main thread interrupted");
+        }
+        System.out.println("Main thread exiting");
+    }
 
     static class NewThreadWithRunnable implements Runnable {
         Thread thread;
