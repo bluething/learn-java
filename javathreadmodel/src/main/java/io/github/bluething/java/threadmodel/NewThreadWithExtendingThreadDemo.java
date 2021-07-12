78aff3a23ae6351b16c697b34693b7f3f708bb01
bluething <habib.gunadarma@gmail.com>
2021-07-12 16:21:53 +0700

    Implement run method

--- javathreadmodel/src/main/java/io/github/bluething/java/threadmodel/NewThreadWithExtendingThreadDemo.java
+++ javathreadmodel/src/main/java/io/github/bluething/java/threadmodel/NewThreadWithExtendingThreadDemo.java
@@ -4,9 +4,22 @@ public class NewThreadWithExtendingThreadDemo {
 
     static class NewThreadWithExtendingThread extends Thread {
 
+        NewThreadWithExtendingThread() {
+            super("Demo thread");
+            System.out.println("Child thread: " + this);
+        }
+
         @Override
         public void run() {
-            super.run();
+            try {
+                for (int i = 5; i > 0; i--) {
+                    System.out.println("Child thread " + i);
+                    Thread.sleep(1000);
+                }
+            } catch (InterruptedException e) {
+                System.out.println("Child thread interrupted");
+            }
+            System.out.println("Exiting child thread");
         }
     }
 }
