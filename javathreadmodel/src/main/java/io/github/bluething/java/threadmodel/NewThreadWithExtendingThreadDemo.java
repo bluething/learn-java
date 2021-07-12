d5ce65ac8dbac32984eea556e8ba31a025905115
bluething <habib.gunadarma@gmail.com>
2021-07-12 16:20:42 +0700

    Extend class Thread

---
+++ javathreadmodel/src/main/java/io/github/bluething/java/threadmodel/NewThreadWithExtendingThreadDemo.java
@@ -0,0 +1,12 @@
+package io.github.bluething.java.threadmodel;
+
+public class NewThreadWithExtendingThreadDemo {
+
+    static class NewThreadWithExtendingThread extends Thread {
+
+        @Override
+        public void run() {
+            super.run();
+        }
+    }
+}
