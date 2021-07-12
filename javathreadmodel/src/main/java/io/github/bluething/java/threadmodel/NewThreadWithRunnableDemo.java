7abf9bfba88334a8978ff7cfb5f24d28755d33d5
bluething <habib.gunadarma@gmail.com>
2021-07-12 15:28:45 +0700

    Instantiate Thread object

--- javathreadmodel/src/main/java/io/github/bluething/java/threadmodel/NewThreadWithRunnableDemo.java
+++ javathreadmodel/src/main/java/io/github/bluething/java/threadmodel/NewThreadWithRunnableDemo.java
@@ -1,7 +1,14 @@
 package io.github.bluething.java.threadmodel;
 
 public class NewThreadWithRunnableDemo {
-    class NewThreadWithRunnable implements Runnable {
+
+    static class NewThreadWithRunnable implements Runnable {
+        Thread thread;
+
+        NewThreadWithRunnable() {
+            this.thread = new Thread(this, "Demo thread");
+            System.out.println("Child thread " + thread);
+        }
 
         @Override
         public void run() {
