bca32d9dd67aba2c7a358b1d38200be82ec52d15
bluething <habib.gunadarma@gmail.com>
2021-07-14 14:10:03 +0700

    Not sync method

---
+++ javathreadmodel/src/main/java/io/github/bluething/java/threadmodel/synchronization/Callme.java
@@ -0,0 +1,13 @@
+package io.github.bluething.java.threadmodel.synchronization;
+
+public class Callme {
+    void call(String msg) {
+        System.out.print("[ " + msg);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            System.out.println("Interrupted");
+        }
+        System.out.println(" ]");
+    }
+}
