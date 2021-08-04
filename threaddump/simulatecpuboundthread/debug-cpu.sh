1a7183d91048a3e0967463a4a143fecdddce17d2
bluething <habib.gunadarma@gmail.com>
2021-08-04 10:17:02 +0700

    Adjust the command to my local

--- threaddump/simulatecpuboundthread/debug-cpu.sh
+++ threaddump/simulatecpuboundthread/debug-cpu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-java -jar simulatecpuboundthread-1.0.0-SNAPSHOT.jar 100000 12 | nid-translator.pl > thread-dumps-w-decimal-nids.out &
+java -jar simulatecpuboundthread-1.0.0-SNAPSHOT.jar 100000 12 | ./nid-translator.pl > thread-dumps-w-decimal-nids.out &
 
 sh dump-threads.sh
 
--- threaddump/simulatecpuboundthread/dump-threads.sh
+++ threaddump/simulatecpuboundthread/dump-threads.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 a=1
 numberOfKills=10
-pid=$(ps aux| grep [j]ava| awk '{print $2}')
+pid=$(ps aux| grep '[s]imulatecpuboundthread-1.0.0-SNAPSHOT.jar'| awk '{print $2}')
 
 echo "Sending kill -3 at PID:" $pid
 
