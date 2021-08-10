5d8bcd12b3d1fc0320e91e7f2182c6b8c3cb379a
bluething <habib.gunadarma@gmail.com>
2021-08-10 10:41:04 +0700

    Add resources

---
+++ heapdump/klassified/src/main/java/io/github/bluething/java/heapdump/klassified/resources/PopulateResource.java
@@ -0,0 +1,73 @@
+package io.github.bluething.java.heapdump.klassified.resources;
+
+import com.codahale.metrics.annotation.Timed;
+import io.dropwizard.hibernate.UnitOfWork;
+import io.github.bluething.java.heapdump.klassified.jdbi.Advert;
+import io.github.bluething.java.heapdump.klassified.jdbi.AdvertDAO;
+import io.github.bluething.java.heapdump.klassified.jdbi.Advertiser;
+import io.github.bluething.java.heapdump.klassified.jdbi.AdvertiserDAO;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+/**
+ * NB: must be thread-safe
+ */
+@Path("/populate")
+public class PopulateResource
+{
+    private static final int POPULATE_FACTOR = 30_000;
+
+    private final AdvertDAO advertDAO;
+    private final AdvertiserDAO advertiserDAO;
+
+    public PopulateResource(final AdvertDAO advertDAO, final AdvertiserDAO advertiserDAO)
+    {
+        this.advertDAO = advertDAO;
+        this.advertiserDAO = advertiserDAO;
+    }
+
+    @GET
+    @Timed
+    @UnitOfWork
+    public String populate()
+    {
+        for (int i = 0; i < POPULATE_FACTOR; i++)
+        {
+            final Advertiser keen = new Advertiser("Keen Kustomer " + i, "123 Fake Street");
+            advertiserDAO.save(keen);
+
+            advertDAO.save(new Advert(
+                "Job",
+                "Java Developer " + i,
+                "Agile, TDD, Spring, Hibernate ... " + i,
+                keen));
+        }
+
+        for (int i = 0; i < POPULATE_FACTOR; i++)
+        {
+            final Advertiser sofaSales = new Advertiser("Refurbisher " + i, "456 Fake Street");
+            advertiserDAO.save(sofaSales);
+
+            advertDAO.save(new Advert(
+                "Furniture",
+                "Second Hand Sofa " + i,
+                "Very soft and comfortable number" + i,
+                sofaSales));
+        }
+
+        for (int i = 0; i < POPULATE_FACTOR; i++)
+        {
+            final Advertiser rentalAgent = new Advertiser("Richard's Rentals " + i, "789 Fake Street");
+            advertiserDAO.save(rentalAgent);
+
+            advertDAO.save(new Advert(
+                "property",
+                "Fantastic Room " + i,
+                "Location, location ... rising damp number" + i,
+                rentalAgent));
+        }
+
+        return "Ok";
+    }
+}
