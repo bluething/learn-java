30f17cbab9e8a66a86e19cccfbeb7d44ab4a9de6
bluething <habib.gunadarma@gmail.com>
2021-08-11 14:28:57 +0700

    Main method
    We implement our class loader.
    We load the classes from heapdump/simulateclassloadermemoryleak/build/classes/java/main, this is the output dir from gradle. It depend on module setting in idea.

--- heapdump/simulateclassloadermemoryleak/src/main/java/io/github/bluething/java/heapdump/simulateclassloadermemoryleak/ApplicationRunner.java
+++ heapdump/simulateclassloadermemoryleak/src/main/java/io/github/bluething/java/heapdump/simulateclassloadermemoryleak/ApplicationRunner.java
@@ -1,7 +1,25 @@
 package io.github.bluething.java.heapdump.simulateclassloadermemoryleak;
 
-public class ApplicationRunner {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.net.URL;
+import java.net.URLClassLoader;
 
+public class ApplicationRunner {
+    public static void main(String[] args) throws IOException, NoSuchMethodException,
+            InvocationTargetException, IllegalAccessException, InstantiationException, ClassNotFoundException {
+        final String pluginName = "io.github.bluething.java.heapdump.simulateclassloadermemoryleak.ExamplePlugin";
+
+        while (true) {
+            System.out.println("Press any key to reload the plugins");
+            System.in.read();
+
+            final URL[] urls = { new File("heapdump/simulateclassloadermemoryleak/build/classes/java/main").toURI().toURL()};
+            final URLClassLoader classLoader = new URLClassLoader(urls, null);
+            final Class<?> aClass = classLoader.loadClass(pluginName);
+            final Object plugin = aClass.newInstance();
+            aClass.getMethod("initialize").invoke(plugin);
+        }
     }
 }
