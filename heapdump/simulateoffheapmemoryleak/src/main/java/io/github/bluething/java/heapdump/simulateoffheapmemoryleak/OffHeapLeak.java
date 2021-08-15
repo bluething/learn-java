66472dc83ff9bc8687e102f9cc354b7dd3fb1ccc
bluething <habib.gunadarma@gmail.com>
2021-08-15 15:19:52 +0700

    Main method

--- heapdump/simulateoffheapmemoryleak/src/main/java/io/github/bluething/java/heapdump/simulateoffheapmemoryleak/OffHeapLeak.java
+++ heapdump/simulateoffheapmemoryleak/src/main/java/io/github/bluething/java/heapdump/simulateoffheapmemoryleak/OffHeapLeak.java
@@ -1,4 +1,20 @@
 package io.github.bluething.java.heapdump.simulateoffheapmemoryleak;
 
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
+
 public class OffHeapLeak {
+    private static final int BUFFER_SIZE = 64 * 1024 * 1024;
+
+    public static void main(String[] args) throws IOException {
+        final List<ByteBuffer> buffers = new ArrayList<>();
+        while (true) {
+            buffers.add(ByteBuffer.allocateDirect(BUFFER_SIZE));
+
+            System.out.println("Press any key to continue");
+            System.in.read();
+        }
+    }
 }
