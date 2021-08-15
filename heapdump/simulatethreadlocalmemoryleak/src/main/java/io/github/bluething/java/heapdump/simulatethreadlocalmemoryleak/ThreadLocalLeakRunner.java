2a280befd4079f6ab0ea9ef66a923a3ae7a48156
bluething <habib.gunadarma@gmail.com>
2021-08-15 15:00:23 +0700

    Press any key to continue the process
    It give us chance to monitor from visualvm

--- heapdump/simulatethreadlocalmemoryleak/src/main/java/io/github/bluething/java/heapdump/simulatethreadlocalmemoryleak/ThreadLocalLeakRunner.java
+++ heapdump/simulatethreadlocalmemoryleak/src/main/java/io/github/bluething/java/heapdump/simulatethreadlocalmemoryleak/ThreadLocalLeakRunner.java
@@ -1,5 +1,6 @@
 package io.github.bluething.java.heapdump.simulatethreadlocalmemoryleak;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
@@ -11,7 +12,10 @@ public class ThreadLocalLeakRunner {
     private static final int POOL_SIZE = 50;
     private static final ExecutorService threadPool = Executors.newFixedThreadPool(POOL_SIZE);
 
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
+    public static void main(String[] args) throws ExecutionException, InterruptedException, IOException {
+        System.out.println("Press any key to continue");
+        System.in.read();
+
         final List<Future<?>> futures = new ArrayList<>();
         for (int i = 0; i < POOL_SIZE; i++) {
             final Future<?> future = threadPool.submit(new LeakingAction());
