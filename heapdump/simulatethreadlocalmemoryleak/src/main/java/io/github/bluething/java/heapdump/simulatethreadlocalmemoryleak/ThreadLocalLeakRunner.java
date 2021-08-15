062d0509940c47c64b87f93e68b7fd61ba7129b0
bluething <habib.gunadarma@gmail.com>
2021-08-15 14:46:53 +0700

    Main method

--- heapdump/simulatethreadlocalmemoryleak/src/main/java/io/github/bluething/java/heapdump/simulatethreadlocalmemoryleak/ThreadLocalLeakRunner.java
+++ heapdump/simulatethreadlocalmemoryleak/src/main/java/io/github/bluething/java/heapdump/simulatethreadlocalmemoryleak/ThreadLocalLeakRunner.java
@@ -1,7 +1,27 @@
 package io.github.bluething.java.heapdump.simulatethreadlocalmemoryleak;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 public class ThreadLocalLeakRunner {
-    public static void main(String[] args) {
-        
+    private static final int POOL_SIZE = 50;
+    private static final ExecutorService threadPool = Executors.newFixedThreadPool(POOL_SIZE);
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        final List<Future<?>> futures = new ArrayList<>();
+        for (int i = 0; i < POOL_SIZE; i++) {
+            final Future<?> future = threadPool.submit(new LeakingAction());
+            futures.add(future);
+        }
+
+        for (Future<?> future : futures) {
+            future.get();
+        }
+
+        System.out.println("Work Complete");
     }
 }
